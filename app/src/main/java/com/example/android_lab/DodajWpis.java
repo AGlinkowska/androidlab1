ef5ce32c7c2b2264f1cb87f80568b39f5b14d6df
AGlinkowska <annglin96@gmail.com>
2019-05-29 00:51:59 +0200

    Poprawne edytowanie wpisów

--- app/src/main/java/com/example/android_lab/DodajWpis.java
+++ app/src/main/java/com/example/android_lab/DodajWpis.java
@@ -10,10 +10,28 @@ import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
+    private int modyfi_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if(extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
+                EditText kolor = (EditText) findViewById(R.id.kolor);
+                EditText wielkość = (EditText) findViewById(R.id.wielkosc);
+                EditText opis = (EditText) findViewById(R.id.opis);
+                kolor.setText(zwierz.getKolor());
+                wielkość.setText( Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+                this.modyfi_id=zwierz.getId();
+            }
+        }catch(Exception ex) {
+            this.modyfi_id = 0;
+        }
 
         ArrayAdapter gatunki = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, new String[] {"Pies", "Kot", "Rybki"});
         Spinner gatunek = (Spinner)findViewById(R.id.gatunek);
@@ -27,6 +45,8 @@ public class DodajWpis extends AppCompatActivity {
         Spinner gatunek = (Spinner)findViewById(R.id.gatunek);
 
         Animal zwierze = new Animal(gatunek.getSelectedItem().toString(),kolor.getText().toString(),Float.valueOf(wielkosc.getText().toString()),opis.getText().toString());
+
+        zwierze.setId(this.modyfi_id);
 
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
